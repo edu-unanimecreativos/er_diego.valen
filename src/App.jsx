02714cc598f36ccdfa7e9bbdd0b2770e232fd02e
edu-unanimecreativos <eduardoalcover@unanimecreativos.com>
2026-05-27 10:12:13 +0200

    use hash router for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Project from './pages/Project'
 import Contact from './pages/Contact'
 
 export default function App() {
   return (
-    <BrowserRouter basename="/er_diego.valen">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/work/:slug" element={<Project />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
